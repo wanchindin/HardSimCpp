71d3d6c9f1284b95de31f449d48e08f4cc11bfc6
丁婉芩 <dingwanqin@dingwanqindeMacBook-Air.local>
2025-04-16 10:17:31 +1000

    initial commit

---
+++ MyVector.hpp
@@ -0,0 +1,30 @@
+#ifndef MYVECTOR_HPP
+#define MYVECTOR_HPP
+
+#include <iostream>
+
+template<typename T>
+class MyVector
+{
+private:
+    T* data;
+    size_t size;
+    size_t capacity;
+    
+public:
+    MyVector();
+    ~MyVector();
+
+    
+};
+
+MyVector::MyVector(/* args */)
+{
+}
+
+MyVector::~MyVector()
+{
+}
+
+
+#endif
